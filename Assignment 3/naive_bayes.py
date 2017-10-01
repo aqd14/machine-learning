893694ecae4b6954e935e3c7dc2457e2bfe3112b
aqd14 <aqd14@msstate.edu>
2017-10-01 16:23:45 -0500

    Complete email spam filter

--- Assignment 3/naive_bayes.py
+++ Assignment 3/naive_bayes.py
@@ -10,6 +10,7 @@ from scipy import misc
 from scipy import sparse as sps
 import matplotlib.pyplot as plt
 
+numTokens = 2500
 
 class MultinomialNaiveBayes():
     def __init__(self):
@@ -25,44 +26,74 @@ class MultinomialNaiveBayes():
         email_word_count = np.sum(train_matrix, 1)      # array-like:     The total word count for each email
         
         # Calculate phi_k|y=1 = p(xj = k|y = 1)
-        self.py_pos = (train_matrix[spam_email_pos].sum() + 1) / (np.sum(email_word_count[spam_email_pos]) + num_tokens)
+        self.py_pos = (train_matrix[spam_email_pos].sum(axis=0) + 1) / (np.sum(email_word_count[spam_email_pos]) + num_tokens)
         # Calculate phi_k|y=0 = p(xj = k|y = 0) 
-        self.py_neg = (train_matrix[nonspam_email_pos].sum() + 1) / (np.sum(email_word_count[nonspam_email_pos]) + num_tokens)
+        self.py_neg = (train_matrix[nonspam_email_pos].sum(axis=0) + 1) / (np.sum(email_word_count[nonspam_email_pos]) + num_tokens)
         
+        # prior
         self.phi_pos = np.count_nonzero(train_labels)/numTrainDocs
         
     def predict(self, test_labels, test_matrix):
         num_test_docs = test_labels.shape[0]
-        log_p_pos = test_matrix.dot(np.log(self.py_pos)) + np.log(self.phi_pos)
-        log_p_neg = test_matrix.dot(np.log(slef.py_neg)) + np.log(1 - self.phi_pos)
-        return log_p_pos > log_p_neg
+#         log_p_pos = np.log(test_matrix.dot(self.py_pos.T)) + np.log(self.phi_pos)
+#         log_p_neg = np.log(test_matrix.dot(self.py_neg.T)) + np.log(1 - self.phi_pos)
+        log_p_pos = test_matrix.dot(np.log(self.py_pos.T)) + np.log(self.phi_pos)
+        log_p_neg = test_matrix.dot(np.log(self.py_neg.T)) + np.log(1 - self.phi_pos)
+        
+        results = log_p_pos > log_p_neg 
+        # Convert from True/False to 1/0
+        return np.squeeze(np.asarray(results.astype(dtype=int)))
 
-def main():
+def train_and_test(files):
+    # Extract parameters
+    train_labels_f = files[0]
+    train_features_f = files[1]
+    test_labels_f = files[2]
+    test_features_f = files[3]
+    
     # Load the labels for the training set
-    train_labels = np.loadtxt('pa3data/train-labels.txt',dtype=int)
+    train_labels = np.loadtxt(train_labels_f,dtype=int)
     # Get the number of training examples from the number of labels
     numTrainDocs = train_labels.shape[0]
     # This is how many words we have in our dictionary
-    numTokens = 2500
     # Load the training set feature information
-    M = np.loadtxt('pa3data/train-features.txt',dtype=int)
+    M = np.loadtxt(train_features_f,dtype=int)
     # Create matrix of training data
     train_matrix = sps.csr_matrix((M[:,2], (M[:,0], M[:,1])), shape=(numTrainDocs, numTokens))
     
     classifier = MultinomialNaiveBayes()
     classifier.fit(train_labels, train_matrix, numTokens)
     
-    test_labels = np.loadtxt('pa3data/test-labels.txt', dtype=int)
+    test_labels = np.loadtxt(test_labels_f, dtype=int)
     # Load the test set feature information
-    N = np.loadtxt('pa3data/test-features.txt',dtype=int)
+    N = np.loadtxt(test_features_f,dtype=int)
     # Create matrix of test data
     test_matrix = sps.csr_matrix((N[:,2], (N[:,0], N[:,1])))
     
     prediction = classifier.predict(test_labels, test_matrix)
     
-    num_wrong_docs = sum(prediction != test_labels)
+    num_wrong_docs = np.sum(prediction != test_labels)
     
-    print(num_wrong_docs)
+    print('Number of wrong classification = {0}'.format(num_wrong_docs))
+    print('Fraction of wrong classification = {0}\n\n'.format(num_wrong_docs/test_labels.shape[0]))
+    
+
+def main():
+    files = ['pa3data/train-labels.txt', 'pa3data/train-features.txt', 'pa3data/test-labels.txt','pa3data/test-features.txt']
+    print('Working with 960-document dataset...')
+    train_and_test(files)
+    
+    files = ['pa3data/train-labels-50.txt', 'pa3data/train-features-50.txt', 'pa3data/test-labels.txt','pa3data/test-features.txt']
+    print('Working with 50-document dataset...')
+    train_and_test(files)
+    
+    files = ['pa3data/train-labels-100.txt', 'pa3data/train-features-100.txt', 'pa3data/test-labels.txt','pa3data/test-features.txt']
+    print('Working with 100-document dataset...')
+    train_and_test(files)
+    
+    files = ['pa3data/train-labels-400.txt', 'pa3data/train-features-400.txt', 'pa3data/test-labels.txt','pa3data/test-features.txt']
+    print('Working with 400-document dataset...')
+    train_and_test(files)
     
 if __name__ == '__main__':
     main()
